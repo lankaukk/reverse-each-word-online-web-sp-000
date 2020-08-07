c01da430590e83930b60b1e16aa9c3bd091c979f
McKayla Lankau <lankaukk@yahoo.com>
2020-08-07 03:57:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
-  sentence.split(" ")
-    sentence.collect do |word|
+  sentence.split(" ") = sentence_array
+  sentence_array.collect do |word|
       puts "#{word.reverse}"
     end
   sentence.join(" ")
